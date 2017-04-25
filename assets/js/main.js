94c597ccf77298c782249a59e7a0112df71c88ee
Mariel Garcia <maryel_565@hotmail.com>
2017-04-25 18:49:09 -0500

    form

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,63 @@ function carrousel() {
     setTimeout(carrousel, 2500);
 }
 carrousel();
+
+
+//VALIDACION FORMULARIO
+window.onload = function(){
+
+	var element = document.querySelector(".form");
+		element.addEventListener("submit", function(event) {
+			event.preventDefault();
+		});
+var inputs=document.getElementsByClassName("form-control");
+var nombre=document.getElementById("name");
+var apellido=document.getElementById("lastname");
+var correo=document.getElementById("fake");
+var mensaje=document.getElementById("commits");
+var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+var letras = function(e){
+		var codigo = e.keyCode;
+		if((codigo>=97 && codigo<=122) || (codigo>=65 && codigo<=90) || codigo==39 || codigo==32){
+			this.nextElementSibling.innerText = "";
+			this.nextElementSibling.style.display = "none"
+			return true;
+		}else{
+			this.nextElementSibling.innerText = "Ingrese sólo letras"
+			this.nextElementSibling.style.display = "inline-block"
+			return false;
+		}
+	}
+
+  nombre.onkeypress=letras;
+  apellido.onkeypress=letras;
+
+
+/*var vEmail=function() {
+  console.log(this.value);
+  if(emailRegex.test(this.value)==true){
+    this.value="";
+    this.nextElementSibling.innerText = "";
+    this.nextElementSibling.style.display = "none";
+    }else {
+      this.nextElementSibling.innerText = "Ingrese E-mail correcto";
+      this.nextElementSibling.style.display = "inline-block";
+      }
+}
+correo.onblur=vEmail*/
+
+var validar = function(){
+		if(this.value.trim().length == 0){
+			this.value = "";
+			this.nextElementSibling.innerText = "Este campo es obligatorio";
+			this.nextElementSibling.style.display = "inline-block";
+		}else{
+			this.nextElementSibling.innerText = "";
+			this.nextElementSibling.style.display = "none";
+		}
+  }
+
+for(var i=0;i<inputs.length;i++){
+			inputs[i].onblur=validar;}
+    }
