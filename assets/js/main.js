dd5ee4d21de80e0d3919d9e1058d3f86a3ff4252
Mariel Garcia <maryel_565@hotmail.com>
2017-04-26 15:53:27 -0500

    terminado.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,3 @@
-<<<<<<< HEAD
-  var contador = 0;
-
-  function carrousel() {
-    var i;
-    var fotos = document.getElementsByClassName("fondo");
-    for (i = 0; i < fotos.length; i++) {
-       fotos[i].style.display = "none";
-    }
-    contador++;
-    if (contador > fotos.length) {contador = 1}
-    fotos[contador-1].style.display = "block";
-    setTimeout(carrousel, 2500);
-  }
-  carrousel();
-
-
-  //VALIDACION FORMULARIO
-  window.onload = function(){
-    var element = document.querySelector(".form");
-    element.addEventListener("submit", function(event) {
-      event.preventDefault();
-  		});
-  var inputs=document.getElementsByClassName("form-control");
-  var nombre=document.getElementById("name");
-  var apellido=document.getElementById("lastname");
-  var mensaje=document.getElementById("commits");
-  var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-
-  var letras = function(e){
-    var codigo = e.keyCode;
-    if((codigo>=97 && codigo<=122) || (codigo>=65 && codigo<=90) || codigo==39 || codigo==32){
-      this.nextElementSibling.innerText = "";
-      this.nextElementSibling.style.display = "none";
-      return true;
-    }else{
-      this.nextElementSibling.innerText = "Ingrese sólo letras";
-      this.nextElementSibling.style.display = "inline-block";
-      return false;
-    }
-  }
-  nombre.onkeypress=letras;
-  apellido.onkeypress=letras;
-
-  var validar = function(){
-    if(this.value.trim().length == 0){
-      this.value = "";
-      this.nextElementSibling.innerText = "Este campo es obligatorio";
-      this.nextElementSibling.style.display = "inline-block";
-    }else{
-      this.nextElementSibling.innerText = "";
-      this.nextElementSibling.style.display = "none";
-    }
-    }
-
-  for(var i=0;i<inputs.length;i++){
-    inputs[i].onblur=validar;}
-    }
-
-document.getElementById("boton").addEventListener("click", display);
-
-function display() {
-    document.getElementById('modal').style.display='block'
-}
-
-document.getElementById("equis").addEventListener("click", equis);
-function equis() {
-    window.scrollTo(0, 0);
-    document.getElementById('modal').style.display='none'
-}
-=======
 //Carrousel
 
 var contador = 0;
@@ -164,4 +93,3 @@ window.onload = function(){
 		inputs[i].onblur=validar;
 	}
 }
->>>>>>> 375a929bdc94c498fa45f3572b59b9832bc6b061
